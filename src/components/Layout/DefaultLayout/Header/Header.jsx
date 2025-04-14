4d1ab5a2329006fce24def1e03b586d8068991e4
lequanglocdev <quanglocdev@gmail.com>
2025-04-14 16:27:21 +0700

    update

--- src/components/Layout/DefaultLayout/Header/Header.jsx
+++ src/components/Layout/DefaultLayout/Header/Header.jsx
@@ -5,7 +5,7 @@ import icons from '@/utils/icon';
 import logo from '@/assets/logo.png';
 import { AuthContext } from '@/components/context/auth.context';
 import { useNavigate } from 'react-router-dom';
-
+import { Link } from 'react-router-dom';
 const Header = () => {
     const { FaRegUserCircle, IoMdLogOut } = icons;
     const { auth, setAuth } = useContext(AuthContext);
@@ -20,12 +20,7 @@ const Header = () => {
         navigate('/'); 
     };
     return (
-        <Navbar
-            bg="dark"
-            variant="dark"
-            expand="lg"
-            className={styles.navbar }
-        >
+        <Navbar bg="dark" variant="dark" expand="lg" className={styles.navbar}>
             <Container>
                 <Navbar.Brand href="/" className={styles.navbarBrand}>
                     <Image src={logo} alt="logo" className={styles.navLogo} />
@@ -33,29 +28,29 @@ const Header = () => {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
                     <Nav>
-                        <Nav.Link href="/" className={styles.navText}>
+                        <Nav.Link as={Link} to="/" className={styles.navText}>
                             TRANG CHỦ
                         </Nav.Link>
-                        <Nav.Link href="/services" className={styles.navText}>
+                        <Nav.Link as={Link} to="/services" className={styles.navText}>
                             DỊCH VỤ
                         </Nav.Link>
-                        <Nav.Link href="/intro" className={styles.navText}>
+                        <Nav.Link as={Link} to="/intro" className={styles.navText}>
                             VỀ CHÚNG TÔI
                         </Nav.Link>
-                        <Nav.Link href="/blog" className={styles.navText}>
+                        <Nav.Link as={Link} to="/blog" className={styles.navText}>
                             TIN TỨC
                         </Nav.Link>
                         {auth.isAuthenticated ? (
                             <>
-                                <Nav.Link  className={styles.navText}>
+                                <Nav.Link className={styles.navText}>
                                     <p>{auth?.user?.email}</p>
                                 </Nav.Link>
                                 <Nav.Link onClick={handleLogout} className={styles.navIcon}>
-                                    <IoMdLogOut size={24} /> 
+                                    <IoMdLogOut size={24} />
                                 </Nav.Link>
                             </>
                         ) : (
-                            <Nav.Link href="/auth" className={styles.navIcon}>
+                            <Nav.Link as={Link} to="/auth" className={styles.navIcon}>
                                 <FaRegUserCircle size={24} />
                             </Nav.Link>
                         )}
